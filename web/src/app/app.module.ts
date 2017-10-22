57401eb23a6e03f61b8aaa905691764e804d3b98
BartoszLewandowski1309 <lewandowskibartosz@icloud.com>
2017-10-22 13:48:24 +0200

    Podstawowy routing, RegisterComponent, LoginComponent

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from "./login.component";
+import { RegisterComponent } from "./register.component";
+
+const appRoutes: Routes = [
+  { path: 'register', component: RegisterComponent},
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
