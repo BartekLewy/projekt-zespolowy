4119cfb6149664dea9797ee90e841f7dec7690b8
BartoszLewandowski1309 <lewandowskibartosz@icloud.com>
2017-10-22 20:44:40 +0200

    Login and Register components

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from "./login.component";
-import { RegisterComponent } from "./register.component";
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
-  { path: 'register', component: RegisterComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     RegisterComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
