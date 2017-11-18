58378ecd2168170ecf80c04706396d1c4be6d84d
BartoszLewandowski1309 <lewandowskibartosz@icloud.com>
2017-11-18 21:39:54 +0100

    Bootstrap 4, Ng-Bootstrap have been added, first view

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-
-import { AuthGuard } from "./auth.guard";
-import { UserService } from "./user.service";
 import { ProductsComponent } from './products/products.component';
 import { CartComponent } from './cart/cart.component';
 import { DiscountComponent } from './discount/discount.component';
 import { HeaderComponent } from './header/header.component';
+
+import { AuthGuard } from "./auth.guard";
+import { UserService } from "./user.service";
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 const appRoutes: Routes = [
   {
@@ -21,6 +22,7 @@ const appRoutes: Routes = [
     canActivate: [AuthGuard],
     path: 'dashboard',
   },
+  { path: '', component: ProductsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent }
 ];
@@ -41,7 +43,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       {enableTracing: true}
-    )
+    ),
+    NgbModule.forRoot()
   ],
   providers: [UserService, AuthGuard],
   bootstrap: [AppComponent]
--- web/src/app/products/products.component.ts
+++ web/src/app/products/products.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  public products = [
+    { id: 1, name: 'Apple MacBook 12\'\' 1.2GHz/8GB/256GB SSD/HD 615 (gwiezdna szarość)', price: '6499', currency: 'PLN'},
+    { id: 2, name: 'Apple MacBook Air 13\'\' 1.8GHz/8GB/128GB SSD/HD 6000 - model 2017', price: '3799', currency: 'PLN'},
+    { id: 3, name: 'Apple MacBook Pro 13\'\' 2.3GHz/8GB/128GB SSD/Iris Plus 640 (gwiezdna szarość)', price: '6799', currency: 'PLN'},
+    { id: 4, name: 'Apple iMac 21.5\'\' 2.3GHz/8GB/1TB/Iris Plus 640', price: '5499', currency: 'PLN'},
+    { id: 5, name: 'Apple iMac 21.5\'\' 2.3GHz/16GB/1TB Fusion Drive/Iris Plus 640', price: '6939', currency: 'PLN'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
